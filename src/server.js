d015f2885c74ac61293b9e3dd9672adc9fd81fb6
Saepuloh <akh.saepuloh@gmail.com>
2022-05-15 01:01:24 +0700

    coba simple

--- src/server.js
+++ src/server.js
@@ -1,35 +1,11 @@
 const express = require('express')
-const dotenv = require('dotenv')
-const cors = require('cors')
-
 const app = express()
-dotenv.config()
-
-let whitelist = ['http://localhost:8080']
-let corsOptions = {
-    origin: function (origin, callback) {
-        if(whitelist.indexOf(origin) !== -1 || !origin){
-            callback(null, true)
-        } else {
-            callback(new Error('Not Allowed by CORS'))
-        }
-    }
-}
-
-app.use(cors(corsOptions))
-app.use(express.json())
-app.use(express.urlencoded({
-    extended: true
-}))
+const port = 3000
 
 app.get('/', (req, res) => {
-    res.json({
-        message: 'server jalan...'
-    })
+  res.send('Hello World!')
 })
 
-const PORT = process.env.APP_PORT || 5000
-
-app.listen(PORT, () => {
-    console.log(`server running on http://localhost:${PORT}`)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
